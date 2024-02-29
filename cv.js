973db280c5eb39dbf5cb5170806c39627fac6bd4
Kelvin Mue <kelvimue@gmail.com>
2024-02-29 21:36:58 +0300

    added interactivity

---
+++ cv.js
@@ -0,0 +1,9 @@
+function changeProgressBarColor() {
+    var progressBar = document.querySelectorAll('.w3-light-grey .w3-container');
+    progressBar.forEach(function(bar) {
+        bar.style.backgroundColor = 'orange';
+    });
+}
+function showMessage() {
+    alert('Hello! This is a message from your CV.');
+}
